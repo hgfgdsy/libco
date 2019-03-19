9b46f20568a12e482c21c82e392b254a1cc971fd
tracer-nju <tracer@nju.edu.cn>
2019-03-19 20:41:38 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 2783b719d23e4985be528201bb0d74a4   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  20:41:38 up  1:02,  1 user,  load average: 0.13, 0.19, 0.24

--- co.c
+++ co.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 #include "co.h"
 
+#define MAX_CO 10
+
+#if defined(__i386__)
+  #define SP "%%esp"
+#elif defined(__x86_64__)
+  #define SP "%%rsp"
+#endif
+
 struct co {
+	uint8_t stack[4096];
+	void *backup;
+	bool state;
+
 };
+
+int cnt=0;
+
+struct co coroutines[MAX_CO];
 
 void co_init() {
 }
 
 struct co* co_start(const char *name, func_t func, void *arg) {
+  int my_temp = my_cnt;
+  asm volatile("mov " SP ", %0; mov %1, " SP :
+		  "=g"(coroutines[my_cnt].backup) :
+		  "g"((coroutines[my_cnt].stack+4096)&(~0xf)));
   func(arg); // Test #2 hangs
-  return NULL;
+  asm volatile("mov %0," SP : : "g"(coroutines[my_temp].backup));
+  return coroutines[my_temp];
 }
 
 void co_yield() {
