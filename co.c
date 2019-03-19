9b419546bc2489e58258355917e1cec237c8cf2f
tracer-nju <tracer@nju.edu.cn>
2019-03-19 21:44:37 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 2783b719d23e4985be528201bb0d74a4   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  21:44:38 up  2:05,  1 user,  load average: 0.36, 0.28, 0.31

--- co.c
+++ co.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <setjmp.h>
 #include "co.h"
 
 #define MAX_CO 10
@@ -15,12 +16,13 @@ struct co {
 	uint8_t stack[4096];
 	void *backup;
 	bool state;
-
+	jmp_buf my_buf;
 };
 
 int my_cnt=0;
 
 struct co coroutines[MAX_CO];
+//struct co *current;
 
 void co_init() {
 }
@@ -32,10 +34,14 @@ struct co* co_start(const char *name, func_t func, void *arg) {
 		  "g"(coroutines[my_cnt].stack+4096));
   func(arg); // Test #2 hangs
   asm volatile("mov %0," SP : : "g"(coroutines[my_temp].backup));
+  printf("%d\n",cnt);
   return (struct co*)(&coroutines[my_temp]);
 }
 
 void co_yield() {
+/*  int my_val = setjump(current->buf);
+  if(val==0){
+*/
 }
 
 void co_wait(struct co *thd) {
