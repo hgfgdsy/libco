5e219c7ecd41cfd7e86b57b3309850151bbf2fa4
tracer-nju <tracer@nju.edu.cn>
2019-03-19 21:55:17 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 2783b719d23e4985be528201bb0d74a4   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  21:55:17 up  2:16,  1 user,  load average: 0.55, 0.33, 0.30

--- tests/main.c
+++ tests/main.c
@@ -97,11 +97,11 @@ static void test_2() {
 
     Queue *queue = q_new();
 
-    struct co *thd1 = co_start("producer-1", producer, queue);
+    struct co *thd1 = co_start("producer-1", producer, queue);printf("can you?\n");
     struct co *thd2 = co_start("producer-2", producer, queue);
     struct co *thd3 = co_start("consumer-1", consumer, queue);
     struct co *thd4 = co_start("consumer-2", consumer, queue);
-printf("can you?\n");
+
     co_wait(thd1);
     co_wait(thd2);
 
