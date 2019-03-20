c93ac3c86c09c6b8efe760d005338165990e73df
tracer-nju <tracer@nju.edu.cn>
2019-03-20 21:16:49 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: e0a04aa59a384009beef31c954a72263   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  21:16:49 up  2:12,  1 user,  load average: 0.23, 0.26, 0.26

--- co.c
+++ co.c
@@ -65,8 +65,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
   waiting[my_cnt].label = my_cnt;
 //  current = waiting[cnt];
   printf("We start!\n");
-  void *my_arg = arg;
-  func(my_arg); // Test #2 hangs
+  func(arg); // Test #2 hangs
   printf("Have you finished?\n");
   printf("Kao a\n");
   current -> state = false;
