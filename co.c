83b6c2c13169da09f8e1b38836e18c11a1acbdb0
tracer-nju <tracer@nju.edu.cn>
2019-03-20 12:18:16 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 70c24ad1c7ff4e93b94808823929c44c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  12:18:16 up  1:18,  1 user,  load average: 0.44, 0.34, 0.33

--- co.c
+++ co.c
@@ -41,7 +41,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
   waiting[my_cnt] = coroutines;
   waiting[my_cnt]->state = true;
   waiting[my_cnt]->label = my_cnt;
-  int i = setjmp(current->my_buf);
+  int i = setjmp(waiting[0]->my_buf);
   if(i==0){
   asm volatile("mov " SP ", %0; mov %1, " SP :
 		  "=g"(coroutines->backup) :
