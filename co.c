609e357c366f254a7aa1f151d0c59972560cc4d7
tracer-nju <tracer@nju.edu.cn>
2019-03-20 20:40:33 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: e0a04aa59a384009beef31c954a72263   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  20:40:33 up  1:36,  1 user,  load average: 0.35, 0.26, 0.20

--- co.c
+++ co.c
@@ -52,6 +52,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
 		  "=g"(waiting[my_cnt].backup) :
 		  "g"(waiting[my_cnt].stack+(1<<12)));
 */
+  printf("%ld",&waiting[my_cnt].stack+(1<<12));
   asm volatile("mov " SP ", %0; mov %1, " SP :
                  "=g"(waiting[my_cnt].backup) :
                  "g"(waiting[my_cnt].stack+(1<<12)));
@@ -82,7 +83,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
 //  return waiting[my_cnt];
   }
   else{
-//  printf("We back!\n");
+  printf("We back!\n");
   return (struct co*)&waiting[my_cnt];
   }
 //   return waiting[my_cnt];
