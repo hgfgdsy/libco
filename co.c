b332b9ca9c047765054b1953a12a501232c282dd
tracer-nju <tracer@nju.edu.cn>
2019-03-20 12:14:23 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 70c24ad1c7ff4e93b94808823929c44c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  12:14:23 up  1:15,  1 user,  load average: 0.29, 0.26, 0.31

--- co.c
+++ co.c
@@ -35,11 +35,13 @@ void co_init() {
 }
 
 struct co* co_start(const char *name, func_t func, void *arg) {
-  my_cnt++;printf("Are you?\n");
-//  int my_temp = my_cnt;
+  my_cnt++;
+  //  int my_temp = my_cnt;
   struct co *coroutines = (struct co*)malloc(sizeof(struct co));
   waiting[my_cnt]->state = true;
   waiting[my_cnt]->label = my_cnt;
+printf("Are you?\n");
+
   int i = setjmp(waiting[0]->my_buf);
   if(i==0){
   asm volatile("mov " SP ", %0; mov %1, " SP :
