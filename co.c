14582e4e083928deb41ce4b6bf94c5b22b41cfac
tracer-nju <tracer@nju.edu.cn>
2019-03-20 12:19:46 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 70c24ad1c7ff4e93b94808823929c44c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  12:19:46 up  1:20,  1 user,  load average: 0.26, 0.31, 0.32

--- co.c
+++ co.c
@@ -37,8 +37,7 @@ void co_init() {
 struct co* co_start(const char *name, func_t func, void *arg) {
   my_cnt++;
   //  int my_temp = my_cnt;
-  struct co *coroutines = (struct co*)malloc(sizeof(struct co));printf("Are you?\n");
-
+  struct co *coroutines = (struct co*)malloc(sizeof(struct co));
   waiting[my_cnt] = coroutines;
   waiting[my_cnt]->state = true;
   waiting[my_cnt]->label = my_cnt;
