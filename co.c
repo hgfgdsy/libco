644cfa422e39fbc79a952c0bfee661907e9e9fd9
tracer-nju <tracer@nju.edu.cn>
2019-03-20 17:12:03 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: ca6db855af274aa8bb30f3cc63cc3c24   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  17:12:03 up  4:20,  1 user,  load average: 1.36, 1.20, 0.79

--- co.c
+++ co.c
@@ -22,14 +22,15 @@ struct co {
 	int label;
 }__attribute__((aligned(16)));
 
-struct co *waiting[40];
+//struct co *waiting[40];
 int my_cnt=1;
 int select1;
 struct co *current;
+struct co waiting[40];
 
 void co_init() {
-  current = waiting[1] = (struct co*)malloc(sizeof(struct co));
-  waiting[1]->label = 1;
+  current = (struct co*)waiting[1];
+  waiting[1].label = 1;
   srand(time(NULL));
 }
 
@@ -40,36 +41,41 @@ struct co* co_start(const char *name, func_t func, void *arg) {
 
   waiting[my_cnt] = coroutines;*/
   //  current = coroutines;
-  waiting[my_cnt] = (struct co*)malloc(sizeof(struct co));printf("Are you?\t%d\n",my_cnt);
+//  waiting[my_cnt] = (struct co*)malloc(sizeof(struct co));printf("Are you?\t%d\n",my_cnt);
 //  waiting[my_cnt]->state = true;
 //  waiting[my_cnt]->label = my_cnt;
-  current = waiting[my_cnt];
-  int i = setjmp(waiting[1]->my_buf);
+  current = (struct co*)waiting[my_cnt];
+  int i = setjmp(waiting[1].my_buf);
   if(i==0){
   asm volatile("mov " SP ", %0; mov %1, " SP :
-		  "=g"(waiting[my_cnt]->backup) :
-		  "g"(waiting[my_cnt]->stack+(1<<15)));
-  waiting[my_cnt]->state = true;
-  waiting[my_cnt]->label = my_cnt;
+		  "=g"(waiting[my_cnt].backup) :
+		  "g"(waiting[my_cnt].stack+(1<<15)));
+  waiting[my_cnt].state = true;
+  waiting[my_cnt].label = my_cnt;
 //  current = waiting[cnt];
   printf("We start!\n");
   func(arg); // Test #2 hangs
   printf("Have you finished?\n");
   printf("Kao a\n");
   current -> state = false;
-  waiting[my_cnt+1] = waiting[current->label];
-  waiting[current->label] = waiting[my_cnt];
-  my_cnt--;
+//  waiting[my_cnt+1] = waiting[current->label];
+//  waiting[current->label] = waiting[my_cnt];
+//  my_cnt--;
+  bool decide = false;
+  int my_temp = current->label;
+  while(!decide){
   select1 = rand()%(my_cnt) +1;
+  decide = waiting[select1].state;
+  }
   current = waiting[select1];
-  longjmp(waiting[select1]->my_buf,waiting[select1]->label); 
-  asm volatile("mov %0," SP : : "g"(waiting[my_cnt+2]->backup));
+  longjmp(waiting[select1].my_buf,waiting[select1].label); 
+  asm volatile("mov %0," SP : : "g"(waiting[my_temp].backup));
 //  longjmp(waiting[select1]->my_buf,waiting[select1]->label);
 //  return waiting[my_cnt];
   }
   else{
   printf("We back!\n");
-  return waiting[my_cnt];
+  return (struct co*)waiting[my_cnt];
   }
 //   return waiting[my_cnt];
 }
@@ -78,10 +84,15 @@ void co_yield() {
   int my_val = setjmp(current->my_buf);
   printf("%d\t%d\n",my_val,current->label);
   if(my_val==0){
-	  int my_select = rand()%(my_cnt)+1;
+	  bool decide=false;
+	  int my_select;
+	  while(!decide){
+	  my_select = rand()%(my_cnt)+1;
+	  decide = waiting[my_select].state;
+	  }
 	  printf("yieldselect=%d\n",my_select);
-	  current = waiting[my_select];
-	  longjmp(waiting[my_select]->my_buf,current->label);
+	  current = (struct co*)waiting[my_select];
+	  longjmp(waiting[my_select].my_buf,current->label);
   }
   else printf("Are you sure?\n");
 //  return ;
@@ -93,11 +104,11 @@ void co_wait(struct co *thd) {
   if(thd->state){
 //	  int se = rand()%(my_cnt-1)+2;
 //	  printf("select=%d\n",se);
-	  current = waiting[thd->label];
-	  longjmp(waiting[thd->label]->my_buf,1);
+	  current = (struct co*)waiting[thd->label];
+	  longjmp(waiting[thd->label].my_buf,1);
   }
   else {
-	  free(thd);
+//	  free(thd);
   }
 }
 
