53c2e041c8ed0bb95b5443d95c3e574b8518335e
tracer-nju <tracer@nju.edu.cn>
2019-03-20 17:13:54 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: ca6db855af274aa8bb30f3cc63cc3c24   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  17:13:54 up  4:22,  1 user,  load average: 1.57, 1.31, 0.88

--- co.c
+++ co.c
@@ -75,7 +75,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
   }
   else{
   printf("We back!\n");
-  return (struct co*)waiting[my_cnt];
+  return (struct co*)&waiting[my_cnt];
   }
 //   return waiting[my_cnt];
 }
