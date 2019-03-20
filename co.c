56a07a6f7edd914072b44ac787320a66c02f6c6c
tracer-nju <tracer@nju.edu.cn>
2019-03-20 15:46:58 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: ca6db855af274aa8bb30f3cc63cc3c24   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  15:46:58 up  2:55,  1 user,  load average: 0.31, 0.34, 0.36

--- co.c
+++ co.c
@@ -48,7 +48,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
   if(i==0){
   asm volatile("mov " SP ", %0; mov %1, " SP :
 		  "=g"(waiting[my_cnt]->backup) :
-		  "g"(waiting[my_cnt]->stack+1<<15));
+		  "g"(waiting[my_cnt]->stack+(1<<15)));
   waiting[my_cnt]->state = true;
   waiting[my_cnt]->label = my_cnt;
 //  current = waiting[cnt];
